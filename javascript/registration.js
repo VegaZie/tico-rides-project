4c1bb6d88f11d1777e84c593bde42c52ce604955
VegaZie <davidgerardovegas29@gmail.com>
2022-04-08 14:14:17 -0600

    Cambios

---
+++ javascript/registration.js
@@ -0,0 +1,7 @@
+function registration(){
+    const name = document.getElementById('name').value;
+    const lastname = document.getElementById('last_name').value;
+    const phone = document.getElementById('phone').value;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+};
