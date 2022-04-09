10e99c2862156aae0465ec1bdfd606de08d7aea6
VegaZie <davidgerardovegas29@gmail.com>
2022-04-09 12:26:07 -0600

    Registro al 50%

--- javascript/registration.js
+++ javascript/registration.js
@@ -4,4 +4,25 @@ function registration(){
     const phone = document.getElementById('phone').value;
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+
+    let userRegister = JSON.parse(localStorage.getItem('usersRegistred'));
+    if (!userRegister) {
+        userRegister = [];
+    }
+
+    const newUser = {
+        name: name,
+        lastname: lastname,
+        phone: phone,
+        username: username,
+        password: password
+    };
+    
+    userRegister.push(newUser);
+    localStorage.setItem("usersRegistred", JSON.stringify(userRegister));
+    window.open("Authentication_Page.html")
+};
+
+function login(){
+
 };
